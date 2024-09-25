1ac6f06f687d73486561e960ef282db07226f333
zpf_glb <2690955288@qq.com>
2024-09-25 11:07:40 +0800

    retry git push origin after error occured

---
+++ gitPush.sh
@@ -0,0 +1,35 @@
+COMMAND=$1
+echo $COMMAND
+
+MAX_RETRIES=10
+
+RETRY_INTERVAL=5
+
+retry_count=0
+
+while true;
+do
+    echo "--------------------"
+    echo "|                  |"
+    echo "|                  |"
+    echo "--------------------"
+    echo "Excuting Command: $COMMAND ,retry_count: $retry_count"
+    eval $COMMAND
+
+    if [ $? -eq 0 ];
+    then
+        echo "$COMMAND executed successfully."
+        exit 0
+    else
+        echo "$COMMAND excution is failed with exit code $?. retrying..."
+        retry_count=$((retry_count + 1))
+
+        if [ $retry_count -ge $MAX_RETRIES ];
+        then
+            echo "Maximum number of retries ($MAX_RETRIES) reached. Exiting."
+            exit 1
+        fi
+
+        sleep $RETRY_INTERVAL
+    fi
+done
