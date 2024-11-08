4cac0cf00ad43cdb2569cf3c7db1b3a1139f448a
zpf <2690955288@qq.com>
2024-11-08 09:27:34 +0800

    enviroment settings

---
+++ mybash/alias.sh
@@ -0,0 +1,10 @@
+alias vim=/usr/local/bin/vim
+alias vi=/usr/local/bin/nvim
+alias python=~/cpython/python
+alias g++12="/usr/local/bin/x86_64-pc-linux-gnu-g++ -std=c++20"
+alias gcc12=/usr/local/bin/x86_64-pc-linux-gnu-gcc
+alias xclip="xclip -selection clipboard"
+alias ctags=/usr/local/bin/ctags
+alias grep=rg
+set -o vi
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
