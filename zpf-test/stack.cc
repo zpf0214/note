d9e64923b65a4bc6b7e746d2d8082ca8bcdec8ad
zpf <zpf@zpf-xmi-laptop>
2025-01-12 23:18:54 +0800

    new file:   stack.cc

---
+++ zpf-test/stack.cc
@@ -0,0 +1,39 @@
+#include<vector>
+#include<cassert>
+
+using namespace std;
+
+template<typename T>
+class Stack {
+    private:
+        vector<T> elems;
+
+    public:
+        void push(T const& elem);
+        void pop();
+        T const& top() const;
+        bool empty() const {
+            return elems.empty();
+        }
+};
+
+template<typename T>
+void Stack<T>::push(T const& elem)
+{
+    elems.push_back(elem);
+}
+
+template<typename T>
+void Stack<T>::pop()
+{
+    assert(!elems.empty());
+    elems.pop_back();
+}
+
+
+template<typename T>
+T const& Stack<T>::top() const
+{
+    assert(!elems.empty());
+    return elems.back();
+}
